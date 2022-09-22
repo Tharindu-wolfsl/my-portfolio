32757510cf420f3c3753c67d3fa41b30a0954ffe
wolfsl <tharindupriyashan18@gmail.com>
2022-09-23 01:10:03 +0530

    Initial-commit

---
+++ src/components/layouts/Team.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+
+const Team = () => {
+  return (
+    <section id="team">
+    		<div className="container">
+    			<div className="row">
+    				<div className="col-md-12">
+    					<h2 className="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s"><span>AWESOME</span> TEAM</h2>
+    				</div>
+    				<div className="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-offset="50" data-wow-delay="1.3s">
+    					<div className="team-wrapper">
+    						<img src="images/team-img1.jpg" className="img-responsive" alt="team img 1"/>
+    							<div className="team-des">
+    								<h4>TRACY</h4>
+    								<span>Designer</span>
+    								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
+    							</div>
+    					</div>
+    				</div>
+    				<div className="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-offset="50" data-wow-delay="1.6s">
+    					<div className="team-wrapper">
+    						<img src="images/team-img2.jpg" className="img-responsive" alt="team img 2"/>
+    							<div className="team-des">
+    								<h4>MARY</h4>
+    								<span>Developer</span>
+    								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
+    							</div>
+    					</div>
+    				</div>
+    				<div className="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-offset="50" data-wow-delay="1.3s">
+    					<div className="team-wrapper">
+    						<img src="images/team-img3.jpg" className="img-responsive" alt="team img 3"/>
+    							<div className="team-des">
+    								<h4>JULIA</h4>
+    								<span>Director</span>
+    								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
+    							</div>
+    					</div>
+    				</div>
+    				<div className="col-md-3 col-sm-6 col-xs-12 wow fadeIn" data-wow-offset="50" data-wow-delay="1.6s">
+    					<div className="team-wrapper">
+    						<img src="images/team-img4.jpg" className="img-responsive" alt="team img 4"/>
+    							<div className="team-des">
+    								<h4>LINDA</h4>
+    								<span>Manager</span>
+    								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
+    							</div>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    	</section>
+  )
+}
+
+export default Team
